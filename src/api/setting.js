37e5c1305cb276f44943b098f4b03be51fccd588
xiaocai <1766607952@qq.com>
2021-06-27 16:44:25 +0800

    setting: addRole and updRole

--- src/api/setting.js
+++ src/api/setting.js
@@ -1,5 +1,5 @@
 import request from '@/utils/request'
-
+// 查询角色
 export const getRoles = (params) => {
   return request({
     url: '/sys/role',
@@ -7,9 +7,26 @@ export const getRoles = (params) => {
     'params': params
   })
 }
+// 删除角色
 export const delRole = (id) => {
   return request({
     url: '/sys/role/' + id,
     method: 'DELETE'
+  })
+}
+// 添加角色
+export const addRole = (from) => {
+  return request({
+    url: '/sys/role/',
+    method: 'POST',
+    data: from
+  })
+}
+// 修改角色
+export const updRole = (from) => {
+  return request({
+    url: '/sys/role/' + from.id,
+    method: 'PUT',
+    data: from
   })
 }
--- src/views/settings/settings.vue
+++ src/views/settings/settings.vue
@@ -7,23 +7,21 @@
           <!-- tabs 标签栏 -->
           <el-tab-pane label="用户管理" name="first">
             <!-- 添加按钮 -->
-            <el-button type="primary">+添加角色</el-button>
+            <el-button type="primary" @click="addRoulll">+添加角色</el-button>
             <!-- 表格 -->
             <el-table :data="roles">
               <el-table-column
                 label="序号"
                 width="120"
-              >
-                <template slot-scope="scope">
-                  {{ (pageParams.page - 1) }}
-                </template>
-              </el-table-column>
+                type="index"
+                :index="indexMethod"
+              />
               <el-table-column label="角色名称" width="240" prop="name" />
               <el-table-column label="描述" prop="description" />
               <el-table-column label="操作">
                 <template slot-scope="scope">
                   <el-button size="small" type="success">分配权限</el-button>
-                  <el-button size="small" type="primary">编辑</el-button>
+                  <el-button size="small" type="primary" @click="hUpdRole(scope.row)">编辑</el-button>
                   <el-button size="small" type="danger" @click="delRole(scope.row.id)">删除</el-button>
                 </template>
               </el-table-column>
@@ -31,7 +29,9 @@
 
             <el-row type="flex" justify="center" align="middle" style="height: 60px">
               <el-pagination
+                background
                 layout="prev, pager, next"
+                :current-page.sync="page"
                 :total="total"
                 :page-size="pageParams.pagesize"
                 @current-change="hCurrentChange"
@@ -41,26 +41,120 @@
         </el-tabs>
       </el-card>
     </div>
+    <!-- 弹层 -->
+    <el-dialog
+      width="30%"
+      :visible.sync="showDialog"
+      :title="isEdit ? '修改角色' : '新增角色'"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+    >
+      <el-form
+        ref="addForm"
+        :model="form"
+        label-width="80px"
+      >
+        <el-form-item label="角色名称">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input v-model="form.description" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="hSubmit">确认</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { getRoles, delRole } from '@/api/setting'
+import { getRoles, delRole, addRole, updRole } from '@/api/setting'
 export default {
+  name: 'Setting',
   data() {
     return {
+      isEdit: false,
       activeName: 'first',
       roles: [],
+      page: 1,
       pageParams: {
         page: 1,
         pagesize: 2
       },
-      total: 0
+      total: 0,
+      form: { name: '', description: '' },
+      showDialog: false
     }
   },
+  computed: {
+    // 是不是最后一条
+    isLastPage: function() {
+      const sum = Math.ceil(this.total / this.pageParams.pagesize)
+      return this.pageParams.page === sum
+    },
+    maxNum: function() {
+      return Math.ceil(this.total / this.pageParams.pagesize)
+    },
+    // 数据是不是满的
+    isLastPageFulled: function() {
+      return (this.total % this.pageParams.pagesize) === 0
+    }
+  },
+
   created() {
     this.loadRoles()
   },
   methods: {
+    addRoulll() {
+      this.isEdit = false
+      this.showDialog = true
+    },
+    // 修改数据
+    hUpdRole(row) {
+      this.isEdit = true
+      this.showDialog = true
+      this.form = { ...row }
+    },
+    // 修改角色
+    async doUpd() {
+      try {
+        const res = await updRole(this.form)
+        console.log(res.data)
+        this.showDialog = false
+        this.$message.success('修改成功')
+        this.loadRoles()
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    // 添加角色
+    async doAdd() {
+      try {
+        const res = await addRole(this.form)
+        console.log(res.data)
+        this.showDialog = false
+        this.$message.success('添加成功')
+        // 向上取整
+        if (this.isLastPageFulled) {
+          this.pageParams.page = this.maxNum + 1
+        } else {
+          this.pageParams.page = this.maxNum
+        }
+        this.loadRoles()
+      } catch (err) {
+        console.log(err)
+        this.$message('添加失败')
+      }
+    },
+    // 增加 / 修改 角色事件
+    hSubmit() {
+      this.$refs.addForm.validate(valid => {
+        if (valid) {
+          this.isEdit ? this.doUpd() : this.doAdd()
+        }
+      })
+    },
     // 查询数据
     async loadRoles() {
       try {
@@ -68,6 +162,7 @@ export default {
         console.log(res)
         this.roles = res.data.rows
         this.total = res.data.total
+        this.page = this.pageParams.page
       } catch (err) {
         console.log(err)
       }
@@ -105,6 +200,10 @@ export default {
       } catch (err) {
         this.$message('删除失败')
       }
+    },
+    // 序号计算
+    indexMethod(index) {
+      return (this.pageParams.page - 1) * 2 + index + 1
     }
   }
 }
