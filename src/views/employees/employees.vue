fde4ef29f3010c662ee727d3711ddf2d87d784a5
xiaocai <1766607952@qq.com>
2021-06-28 09:57:30 +0800

    PageTool: module  create

--- src/views/employees/employees.vue
+++ src/views/employees/employees.vue
@@ -4,7 +4,28 @@
       <el-card>
         <!-- 具体页面结构 -->
         员工
+        <PageTool>
+          <template #left>总记录20条</template>
+          <template #right>
+            <el-button>导入excel</el-button>
+          </template>
+
+        </PageTool>
       </el-card>
     </div>
   </div>
 </template>
+
+<script>
+import PageTool from '@/components/PageTool'
+export default {
+  components: {
+    PageTool
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
